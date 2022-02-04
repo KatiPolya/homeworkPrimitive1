24fe4cd5d33a1ce4fe79d3744adfb9b4ff4f7b92
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-02-04 22:27:08 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 190;
+        int oneMileAmount = 20;
+        int bonus = ticketPrice / oneMileAmount;
+        System.out.println("Количество бонусных миль:" + bonus);
+    }
+}
